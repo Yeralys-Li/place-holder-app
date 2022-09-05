af50a38e6089128b0bcaf3283dcfe8c99ed0f2d1
Yeralys <yeralysli971228@gmail.com>
2022-09-05 18:29:24 -0400

    *Add permission of internet *Add news models *Add news strings

---
+++ app/src/main/java/cu/lidev/placeholderapp/domain/model/Comment.kt
@@ -0,0 +1,14 @@
+package cu.lidev.placeholderapp.domain.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Comment(
+    @SerializedName("postId") val userId: Int,
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("body") val body: String,
+) : Parcelable
